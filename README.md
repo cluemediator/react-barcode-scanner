2fb4b910597301b22da1f4de2f31915b82766caa
Clue Mediator <cluemediator@gmail.com>
2021-06-13 19:24:49 +0530

    Implement demo

---
+++ README.md
@@ -0,0 +1,24 @@
+# react-barcode-scanner
+
+Barcode scanner in React
+
+## Documentation
+
+[https://www.cluemediator.com/barcode-scanner-in-react](https://www.cluemediator.com/barcode-scanner-in-react)
+
+## Quick Start
+
+Follow the below steps to run the project.
+
+1. Clone repository
+2. Run `npm i` command to install dependencies
+3. Execute `npm start` command to run the project
+
+## Connect with us
+
+Website: [Clue Mediator](https://www.cluemediator.com)  
+Like us on [Facebook](https://www.facebook.com/thecluemediator)  
+Follow us on [Twitter](https://twitter.com/cluemediator)  
+Join us on [Telegram](https://t.me/cluemediator)  
+Subscribe us on [YouTube](https://www.youtube.com/ClueMediator)  
+Follow us on [Instagram](https://www.instagram.com/clue_mediator)
---
+++ src/App.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Quagga from 'quagga';
+
+const App = (props) => {
+
+  const firstUpdate = useRef(true);
+  const [isStart, setIsStart] = useState(false);
+  const [barcode, setBarcode] = useState('');
+
+  useEffect(() => {
+    return () => {
+      if (isStart) stopScanner();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+
+    if (isStart) startScanner();
+    else stopScanner();
+  }, [isStart]);
+
+  const _onDetected = res => {
+    // stopScanner();
+    setBarcode(res.codeResult.code);
+  };
+
+  const startScanner = () => {
+    Quagga.init(
+      {
+        inputStream: {
+          type: 'LiveStream',
+          target: document.querySelector('#scanner-container'),
+          constraints: {
+            facingMode: 'environment' // or user
+          }
+        },
+        numOfWorkers: navigator.hardwareConcurrency,
+        locate: true,
+        frequency: 1,
+        debug: {
+          drawBoundingBox: true,
+          showFrequency: true,
+          drawScanline: true,
+          showPattern: true
+        },
+        multiple: false,
+        locator: {
+          halfSample: false,
+          patchSize: 'large', // x-small, small, medium, large, x-large
+          debug: {
+            showCanvas: false,
+            showPatches: false,
+            showFoundPatches: false,
+            showSkeleton: false,
+            showLabels: false,
+            showPatchLabels: false,
+            showRemainingPatchLabels: false,
+            boxFromPatches: {
+              showTransformed: false,
+              showTransformedBox: false,
+              showBB: false
+            }
+          }
+        },
+        decoder: {
+          readers: props.readers
+        }
+      },
+      err => {
+        if (err) {
+          return console.log(err);
+        }
+        Quagga.start();
+      }
+    );
+    Quagga.onDetected(_onDetected);
+    Quagga.onProcessed(result => {
+      let drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            parseInt(drawingCanvas.getAttribute('width')),
+            parseInt(drawingCanvas.getAttribute('height'))
+          );
+          result.boxes.filter(box => box !== result.box).forEach(box => {
+            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+              color: 'green',
+              lineWidth: 2
+            });
+          });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: '#00F', lineWidth: 2 });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+        }
+      }
+    });
+  };
+
+  const stopScanner = () => {
+    Quagga.offProcessed();
+    Quagga.offDetected();
+    Quagga.stop();
+  };
+
+  return <div>
+    <h3>Barcode scanner in React - <a href="https://www.cluemediator.com/" target="_blank">Clue Mediator</a></h3>
+    <button onClick={() => setIsStart(prevStart => !prevStart)} style={{ marginBottom: 20 }}>{isStart ? 'Stop' : 'Start'}</button>
+    {isStart && <React.Fragment>
+      <div id="scanner-container" />
+      <span>Barcode: {barcode}</span>
+    </React.Fragment>}
+  </div>
+}
+
+export default App;
