6aa144894deddf6444e94059136b514f2aa2e285
Clue Mediator <cluemediator@gmail.com>
2021-06-14 11:05:34 +0530

    update readers

--- src/App.js
+++ src/App.js
@@ -67,7 +67,20 @@ const App = (props) => {
           }
         },
         decoder: {
-          readers: props.readers
+          readers: [
+            'code_128_reader',
+            'ean_reader',
+            'ean_8_reader',
+            'code_39_reader',
+            'code_39_vin_reader',
+            'codabar_reader',
+            'upc_reader',
+            'upc_e_reader',
+            'i2of5_reader',
+            'i2of5_reader',
+            '2of5_reader',
+            'code_93_reader'
+          ]
         }
       },
       err => {
